ebcdc2b46d4632831953406164359bbac6dfacc8
ImadPro69 <imadeddine0.0es@gmail.com>
2023-06-07 01:10:43 +0000

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'), ord('z')+1):
-    if chr(letter) not in ['q', 'e']:
-        print(chr(letter), end='')
-
-print()
+for i in range(97, 123):
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end='')
