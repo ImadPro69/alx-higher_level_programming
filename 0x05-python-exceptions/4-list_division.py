7d529339694392be1ff09e931bc12a58419edc39
ImadPro69 <imadeddine0.0es@gmail.com>
2023-06-26 12:17:57 +0000

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,14 +11,17 @@ def list_division(my_list_1, my_list_2, list_length):
                 if my_list_2[i] == 0:
                     raise ZeroDivisionError("division by 0")
                 quotient = my_list_1[i] / my_list_2[i]
+            else:
+                raise IndexError("out of range")
             result.append(quotient)
         except ZeroDivisionError:
+            result.append(0)
             print("division by 0")
-            result.append(0)
         except TypeError:
+            result.append(0)
             print("wrong type")
-            result.append(0)
         except IndexError:
-            print("out of range")
             result.append(0)
+            print("out of range")
     return result
+
