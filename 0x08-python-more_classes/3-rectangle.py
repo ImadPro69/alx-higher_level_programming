af848943ec6ed9dd29a0ecea3e771a4f0c0e9afb
ImadPro69 <125612764+ImadPro69@users.noreply.github.com>
2023-07-03 15:48:30 +0100

    all task

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -37,3 +37,21 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        '''Defining a method to get the area of the Rectangle'''
+        return (self.height * self.width)
+
+    def perimeter(self):
+        '''Defining a method to get the perimeter of the Rectangle'''
+        if self.height == 0 or self.width == 0:
+            return 0
+        return ((self.height + self.width) * 2)
+
+    def __str__(self):
+        '''Defining __str__ Method'''
+        if self.height == 0 or self.width == 0:
+            return ""
+        else:
+            printable = ((("#" * self.width) + "\n") * self.height)
+            return (printable[:-1])
