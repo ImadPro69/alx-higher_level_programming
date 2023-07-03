94e5f395f2ea21c7c55d1c0a7fb67685d65314ef
ImadPro69 <125612764+ImadPro69@users.noreply.github.com>
2023-07-03 17:06:00 +0200

    Update task 5

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-
-'''Here we learn more about Classes and Objects'''
-
-
+'''Here We Learn More About Classes And Objects'''
 class Rectangle:
     '''Class Rectangle'''
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        '''Initializing atributes'''
+        '''Initializing Atributes'''
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        '''Getting the value of width'''
+        '''Getting The Value Of Width'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''Setting the value of width'''
+        '''Setting The Value Of Width'''
         if not type(value) is int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,12 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
-        '''Getting the value of height'''
+        '''Getting The Value Of Height'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''Setting the value of height'''
+        '''Setting The Value Of Height'''
         if not type(value) is int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -42,11 +39,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        '''Defining a method to get the area of the Rectangle'''
+        '''Defining a Method To Get The Area Of The Rectangle'''
         return (self.height * self.width)
 
     def perimeter(self):
-        '''Defining a method to get the perimeter of the Rectangle'''
+        '''Defining a Method To Get The Perimeter Of The Rectangle'''
         if self.height == 0 or self.width == 0:
             return 0
         return ((self.height + self.width) * 2)
