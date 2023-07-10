8a9293292f5ac1429c2c99d7f4056031d9ffb64b
ImadPro69 <imadeddine0.0es@gmail.com>
2023-07-10 23:59:01 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        sorted_list = sorted(self)
+        print(sorted_list)
+
