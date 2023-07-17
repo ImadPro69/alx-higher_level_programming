007e0a1b8b5a8435cef9b426bdd1b30c3bc41198
ImadPro69 <125612764+ImadPro69@users.noreply.github.com>
2023-07-18 01:51:09 +0200

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,16 +10,16 @@ class Rectangle(Base):
         """Initialize a new Rectangle.
 
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
+            width (int): The width Of the new Rectangle.
+            height (int): The height Of the new Rectangle.
+            x (int): The x coordinate Of the new Rectangle.
+            y (int): The y coordinate Of the new Rectangle.
+            id (int): The identity Of the new Rectangle.
         Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
+            TypeError: If either Of width or height is not an int.
+            ValueError: If either Of width or height <= 0.
+            TypeError: If either Of x or y is not an int.
+            ValueError: If either Of x or y < 0.
         """
         self.width = width
         self.height = height
@@ -29,7 +29,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
+        """Set/get the width Of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -42,7 +42,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
+        """Set/get the height Of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -55,7 +55,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Set/get the x coordinate of the Rectangle."""
+        """Set/get the x coordinate Of the Rectangle."""
         return self.__x
 
     @x.setter
@@ -68,7 +68,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Set/get the y coordinate of the Rectangle."""
+        """Set/get the y coordinate Of the Rectangle."""
         return self.__y
 
     @y.setter
@@ -80,11 +80,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """Return the area of the Rectangle."""
+        """Return the area Of the Rectangle."""
         return self.width * self.height
 
     def display(self):
-        """Print the Rectangle using the `#` character."""
+        """Print the Rectangle Using the `#` character."""
         if self.width == 0 or self.height == 0:
             print("")
             return
@@ -100,11 +100,11 @@ class Rectangle(Base):
 
         Args:
             *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents width attribute
-                - 3rd argument represent height attribute
-                - 4th argument represents x attribute
-                - 5th argument represents y attribute
+                - 1st argument Represents id attribute
+                - 2nd argument Represents width attribute
+                - 3rd argument Represent height attribute
+                - 4th argument Represents x attribute
+                - 5th argument Represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
@@ -142,7 +142,7 @@ class Rectangle(Base):
                     self.y = v
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Rectangle."""
+        """Return the dictionary Representation of a Rectangle."""
         return {
             "id": self.id,
             "width": self.width,
