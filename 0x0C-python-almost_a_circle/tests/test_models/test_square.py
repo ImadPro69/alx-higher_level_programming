a859227fd5d8a6ca57761fb64e9506ce2cd2fd59
ImadPro69 <125612764+ImadPro69@users.noreply.github.com>
2023-07-18 01:47:23 +0200

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,19 +2,17 @@
 # test_square.py
 # Brennan D Baraban <375@holbertonschool.com>
 """Defines unittests for models/square.py.
-
 Unittest classes:
-    TestSquare_instantiation - line 24
-    TestSquare_size - line 88
-    TestSquare_x - line 166
-    TestSquare_y - line 238
-    TestSquare_order_of_initialization - line 306
-    TestSquare_area - line 322
-    TestSquare_stdout - line 343
-    TestSquare_update_args - line 426
-    TestSquare_update_kwargs - line 538
-    TestSquare_to_dictionary - 640
-"""
+    TestSquare_instantiation             line 24
+    TestSquare_size                      line 88
+    TestSquare_x                         line 166
+    TestSquare_y                         line 238
+    TestSquare_order_of_initialization   line 306
+    TestSquare_area                      line 322
+    TestSquare_stdout                    line 343
+    TestSquare_update_args               line 426
+    TestSquare_update_kwargs             line 538
+    TestSquare_to_dictionary             line 640   """
 import io
 import sys
 import unittest
@@ -23,7 +21,7 @@ from models.square import Square
 
 
 class TestSquare_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Square class."""
+    """Unittests for testing instantiation of the Square Class."""
 
     def test_is_base(self):
         self.assertIsInstance(Square(10), Base)
