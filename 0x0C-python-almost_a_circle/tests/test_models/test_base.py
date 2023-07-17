2338024546afa0b9085915dd519992c9523c191f
ImadPro69 <125612764+ImadPro69@users.noreply.github.com>
2023-07-18 01:43:56 +0200

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Defines unittests for base.py.
-
+"""Defines Unittests For base.py.
 Unittest classes:
-    TestBase_instantiation - line 21
-    TestBase_to_json_string - line 108
-    TestBase_save_to_file - line 154
-    TestBase_from_json_string - line 232
-    TestBase_create - line 286
-    TestBase_load_from_file - line 338
-    TestBase_save_to_file_csv - line 404
-    TestBase_load_from_file_csv - line 482
-"""
+    TestBase_instantiation        line 21
+    TestBase_to_json_string       line 108
+    TestBase_save_to_file         line 154
+    TestBase_from_json_string     line 232
+    TestBase_create               line 286
+    TestBase_load_from_file       line 338
+    TestBase_save_to_file_csv     line 404
+    TestBase_load_from_file_csv   line 482   """
 import os
 import unittest
 from models.base import Base
@@ -19,7 +17,7 @@ from models.square import Square
 
 
 class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+    """Unittests for Testing instantiation of the Base Class."""
 
     def test_no_arg(self):
         b1 = Base()
