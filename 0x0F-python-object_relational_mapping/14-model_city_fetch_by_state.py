96b0eb67c75acf84cb3e7cceb92344c7baa4bc5f
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-09-17 23:21:20 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Prints all City objects and their State in a database.
+'''
+Prints all City objects And their State in a database.
 '''
 import sys
 from sqlalchemy import create_engine
