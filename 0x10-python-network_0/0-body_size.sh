a0ce992eb191e7c4de792efd74da0cfce77b7ea1
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-09-29 00:23:33 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Get the URL from the user input
-URL=$1
-
-# Send a request to the URL using curl
-RESPONSE=$(curl -sL "$URL")
-
-# Get the size of the response body in bytes
-BODY_SIZE=$(wc -c <<< "$RESPONSE")
-
-# Display the size of the response body
-echo "$BODY_SIZE"
+curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2
