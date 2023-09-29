46e0280475a3b433cba8b4de5eb8f04959dca131
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-09-30 00:43:32 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Retrieves the last 10 commits of a repository.
-Usage: ./100-github_commits.py repository_name repository_owner_name
-"""
+"""Hi"""
 import requests
 import sys
 
